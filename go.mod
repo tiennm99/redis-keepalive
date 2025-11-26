35340c3543d68eee63f5e9f968f6071f2ab6b862
tiennm99 <tiennm99@outlook.com>
2025-11-26 20:46:40 +0700

    feat: init

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/tiennm99/redis-keepalive
+
+go 1.23.12
+
+toolchain go1.24.10
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/redis/go-redis/v9 v9.17.1
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
+	redisUrl, isExist := os.LookupEnv("REDIS_URL")
+	if !isExist {
+		log.Fatal("Warning: REDIS_URL not set!")
+		return
+	}
+
+	opt, err := redis.ParseURL(redisUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	rdb := redis.NewClient(opt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		ticker := time.NewTicker(time.Minute)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				if err := incrementCounter(ctx, rdb); err != nil {
+					log.Printf("Keepalive increment error: %v", err)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	defer func() {
+		cancel()
+		if err := rdb.Close(); err != nil {
+			log.Printf("Error closing cluster: %v", err)
+		}
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+}
+
+func incrementCounter(ctx context.Context, rdb *redis.Client) error {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+	counterDocId := "counter"
+	increment, err := rdb.Incr(ctx, counterDocId).Result()
+	if err != nil {
+		return err
+	}
+	log.Printf("Counter : %d\n", increment)
+	return nil
+}
